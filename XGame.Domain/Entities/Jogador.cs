128b199ee3b187ec4a2ca5d48461a90b1377d70f
paulo <paulo@DESKTOP-DRG1UOR>
2024-03-07 13:30:53 -0800

    Add project files.

---
+++ XGame.Domain/Entities/Jogador.cs
@@ -0,0 +1,18 @@
+﻿using XGame.Domain.ValueObjects;
+using XGame.Domain.Enum;
+
+namespace XGame.Domain.Entities
+{
+    public class Jogador
+    {
+        public Guid Id { get; set; }
+
+        public Nome Nome { get; set; }
+
+        public Email Email { get; set; }
+
+        public string Senha { get; set; }
+
+        public EnumSituacaoJogador Status { get; set; }
+    }
+}
---
+++ XGame.Domain/Interfaces/Services/IServiceJogador.cs
@@ -0,0 +1,11 @@
+﻿using XGame.Domain.ValueObjects;
+
+namespace XGame.Domain.Interfaces.Services
+{
+    public interface IServiceJogador
+    {
+        string AutenticarJogador(string email, string senha);
+
+        Guid AdicionarJogador(Nome nome, Email email, string senha);
+    }
+}
